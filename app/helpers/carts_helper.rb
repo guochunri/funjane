9250f6e029d0c1d5565f5fa42dc4d94dd6a62fa7
郭春日 <uochunri@gmail.com>
2017-08-10 15:19:57 +0800

    Implement cart_total_price

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,8 @@
 module CartsHelper
+
+  # 计算购物车商品总价
+  def render_cart_total_price(cart)
+    cart.total_price
+  end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,11 +2,23 @@ class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items, source: :product
 
-
+  # 加入购物车
   def add_product_to_cart(product)
     ci = cart_items.build
     ci.product = product
     ci.quantity = 1
     ci.save
   end
+
+  # 购物车总价
+  def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.product.price.present?
+        sum += cart_item.quantity * cart_item.product.price
+      end
+    end
+    sum
+  end
+  
 end
