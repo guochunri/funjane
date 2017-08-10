9c43a27114420d6255eff17d9707359fae897e27
郭春日 <uochunri@gmail.com>
2017-08-10 11:22:49 +0800

    slim index:brand/category/product

--- app/helpers/admin/brands_helper.rb
+++ app/helpers/admin/brands_helper.rb
@@ -1,10 +1,20 @@
 module Admin::BrandsHelper
 
+  #显示 Publish/Hidden 状态
   def render_brand_status(brand)
     if brand.is_hidden
       content_tag(:span, "", :class => "fa fa-lock fa-fw")
     else
       content_tag(:span, "", :class => "fa fa-globe fa-fw")
+    end
+  end
+
+  #显示 Pbulish/Hidden 按钮
+  def render_brand_publish_or_hide(brand)
+    if brand.is_hidden
+      link_to(t('btn-publish'), publish_admin_brand_path(brand), :method => :post, :class => 'btn btn-sm btn-default')
+    else
+      link_to(t('btn-hide'), hide_admin_brand_path(brand), :method => :post, :class => 'btn btn-sm btn-default')
     end
   end
 
--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,10 +1,20 @@
 module Admin::ProductsHelper
 
+  #显示 Publish/Hidden 状态
   def render_product_status(product)
     if product.is_hidden
       content_tag(:span, "", :class => "fa fa-lock fa-fw")
     else
       content_tag(:span, "", :class => "fa fa-globe fa-fw")
+    end
+  end
+
+  #显示 Pbulish/Hidden 按钮
+  def render_product_publish_or_hide(product)
+    if product.is_hidden
+      link_to(t('btn-publish'), publish_admin_product_path(product), :method => :post, :class => 'btn btn-sm btn-default')
+    else
+      link_to(t('btn-hide'), hide_admin_product_path(product), :method => :post, :class => 'btn btn-sm btn-default')
     end
   end
 
