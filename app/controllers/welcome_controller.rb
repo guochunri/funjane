5b55de385c37f527faee860b4336240c1e8fd079
郭春日 <uochunri@gmail.com>
2017-08-11 20:42:45 +0800

    Landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,31 @@
 class WelcomeController < ApplicationController
+  layout "welcome"
 
   def index
     @intros = Intro.published
+    
+    # 商品类型 / 品牌
+    @category_groups = CategoryGroup.published
+    @brands = Brand.published
+
+    # 判断是否筛选分类
+    if params[:category].present?
+      @category = params[:category]
+      @category_id = Category.find_by(name: @category)
+
+      @products = Product.where(:category => @category_id).published.paginate(:page => params[:page], :per_page => 12)
+
+    # 判断是否筛选品牌
+    elsif params[:brand].present?
+      @brand = params[:brand]
+      @brand_id = Brand.find_by(name: @brand)
+
+      @products = Product.where(:brand => @brand_id).published.paginate(:page => params[:page], :per_page => 12)
+
+    # 预设显示所有公开商品
+    else
+      @products = Product.published.paginate(:page => params[:page], :per_page => 12)
+    end
   end
 
 end
