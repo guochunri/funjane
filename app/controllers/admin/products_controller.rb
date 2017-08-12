a309788e1fcb002c6d542184644df786f865bfcf
郭春日 <uochunri@gmail.com>
2017-08-12 17:52:29 +0800

    Setup admin/product/index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,7 +4,7 @@ class Admin::ProductsController < ApplicationController
   layout "admin"
 
   def index
-    @products = Product.all.recent
+    @products = Product.all.recent.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
